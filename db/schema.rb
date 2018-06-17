52d324cc939dedfab4ae1b6a72636b27bee72a52
Asfandyar Malik <asfandyarashraf.malik@cubic.com>
2018-06-17 03:44:55 +0200

    Adding Image to Attachments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_16_005551) do
+ActiveRecord::Schema.define(version: 2018_06_17_014051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 2018_06_16_005551) do
     t.bigint "invoice_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["invoice_id"], name: "index_attachments_on_invoice_id"
   end
 
