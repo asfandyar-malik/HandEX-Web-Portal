e9d5c824d381cca22ec3c66724bb5a953c933924
Asfandyar Malik <asfandyarashraf.malik@cubic.com>
2018-06-26 12:09:39 +0200

    trade_info update addded

--- db/migrate/20180625134432_add_user_to_trade_info.rb
+++ db/migrate/20180625134432_add_user_to_trade_info.rb
@@ -1,7 +1,5 @@
 class AddUserToTradeInfo < ActiveRecord::Migration[5.2]
   def change
-      create_table :tradeinfos do |t|
-          t.references :user, foreign_key: true
-      end
+      add_reference :tradeinfos, :user, foreign_key: true
   end
-end
+end 
