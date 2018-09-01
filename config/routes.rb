4603113867ac0b72647356b74f25267fe8e22b51
Asfandyar Malik <asfandyarashraf.malik@cubic.com>
2018-09-01 15:43:45 +0200

    impressum added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
+  get 'pages/impressum' => 'pages#impressum'
+
   devise_for :users,
              path: '',
              path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
