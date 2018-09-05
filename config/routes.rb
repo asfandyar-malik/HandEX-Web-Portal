7f516f3a5466eed7a2de8e82137a1abd2eb75ba3
Asfandyar Malik <asfandyarashraf.malik@cubic.com>
2018-09-06 01:58:30 +0200

    Good progress with new design

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get 'pages/impressum' => 'pages#impressum'
+  get 'pages/termsAndConditions' => 'pages#termsAndConditions'
+
+
 
   devise_for :users,
              path: '',
