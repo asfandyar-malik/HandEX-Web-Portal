24141a05b57bf7fc54b606b7577b234394d263d6
Asfandyar Malik <asfandyarashraf.malik@cubic.com>
2018-10-17 21:16:52 +0200

    remove useless db migration

--- db/migrate/20181012162647_drop_invoices_table.rb
+++
@@ -1,9 +0,0 @@
-class DropInvoicesTable < ActiveRecord::Migration[5.2]
-  def up
-    drop_table :invoices
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
--- db/migrate/20181012162654_drop_attachments_table.rb
+++
@@ -1,9 +0,0 @@
-class DropAttachmentsTable < ActiveRecord::Migration[5.2]
-  def up
-    drop_table :attachments
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
