e528d187e81184f81e9783bf05a2a4a834c3e660
Utkrist Adhikari <utkrist@outlook.com>
2019-02-06 13:51:26 +0100

    All migrations refactored to Insurances From Listing

--- db/migrate/20181111090256_add_new_columns_to_insurances.rb
+++ db/migrate/20181111090256_add_new_columns_to_insurances.rb
@@ -5,7 +5,7 @@ class AddNewColumnsToListings < ActiveRecord::Migration[5.2]
     add_column :insurances, :ak_number, :string
     add_column :insurances, :company_name, :string
     add_column :insurances, :tax_number, :string
-    add_column :insurances, :years_trading_without_hermes_cover, :string
+    add_column :insurances, :years_trading_without_hermes_cover, F:string
     add_column :insurances, :experience_with_export_country, :string
     add_column :insurances, :adequate_claims_management, :string
     add_column :insurances, :agreed_payments_output, :string
