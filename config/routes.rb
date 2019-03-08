5e0432728cde9bfca9c373597a7c9fec8e89ba01
Utkrist Adhikari <utkrist@handex.co>
2019-03-08 17:23:56 +0100

    BACK-228: HTML Structure setup for insurance.

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,19 @@
 Rails.application.routes.draw do
-    
+
     root to: 'pages#home'
-    
+
     get 'pages/landing' => 'pages#landing'
     get 'pages/impressum' => 'pages#impressum'
     get 'pages/termsAndConditions' => 'pages#termsAndConditions'
     get 'pages/applicationProcessing' => 'pages#applicationProcessing'
     get 'pages/guidelines' => 'pages#guidelines'
 
-    # resources :insurances
-    # get 'insurances/index' => 'insurances#index'
 
     resources :insurances do
       member do
         get 'submitted_applications'
         get 'approved_applications'
+        get 'summary_for_user'
       end
     end
 
@@ -22,6 +21,6 @@ Rails.application.routes.draw do
                path:        '',
                path_names:  {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
                controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
-    
+
     resources :users, only: [:show]
 end
