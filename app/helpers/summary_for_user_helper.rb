26dd450b4fdf202a7b8fc2f27683ab49e12fdacd
Utkrist Adhikari <utkrist@handex.co>
2019-03-11 17:45:33 +0100

    BACK-210 #time 2h Refactor files into folders for better understading of the structure

--- app/helpers/summary_for_user_helper.rb
+++ app/helpers/summary_for_user_helper.rb
@@ -1,8 +1,8 @@
 module SummaryForUserHelper
-  def check_missing(answer)
+  def check_missing(answer, error_message=nil)
     if answer == nil || answer.empty?
       content_tag(:div, class: "text-red") do
-        "..."
+        error_message || '...'
       end
     else
       content_tag(:div, class: "text-babu") do
