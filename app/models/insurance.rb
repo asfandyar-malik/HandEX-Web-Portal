8a78da5273756aa7d11232c283e9e137b7d26043
Asfandyar Malik <asfandyarashraf@hotmail.com>
2019-03-18 14:20:54 +0100

    BACK-203 #time 3h adding validation on client and server side for terms and conditions

--- app/models/insurance.rb
+++ app/models/insurance.rb
@@ -1,3 +1,8 @@
 class Insurance < ApplicationRecord
     belongs_to :user
+
+    validates :read_all_instructions, :acceptance => true
+    validates :read_privacy_policy, :acceptance => true
+    validates :accept_terms_conditions, :acceptance => true
+    
 end
