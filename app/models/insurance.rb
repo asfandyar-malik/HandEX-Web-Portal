fe872cb2157fcba83b196a0cef2e2790e1f117bf
Asfandyar Malik <asfandyarashraf@hotmail.com>
2019-03-18 17:13:32 +0100

    BACK-109 #time 3h File upload works perfectly

--- app/models/insurance.rb
+++ app/models/insurance.rb
@@ -4,5 +4,23 @@ class Insurance < ApplicationRecord
     validates :read_all_instructions, :acceptance => true
     validates :read_privacy_policy, :acceptance => true
     validates :accept_terms_conditions, :acceptance => true
+
+    has_attached_file :self_disclosure, styles: { medium: "300x300>", thumb: "100x100>" }
+    validates_attachment_content_type :self_disclosure, content_type: ['image/jpeg', 'image/png', 'image/gif', 'application/pdf']
+
+    has_attached_file :annual_financial_statement_importer, styles: { medium: "300x300>", thumb: "100x100>" }
+    validates_attachment_content_type :annual_financial_statement_importer, content_type: ['image/jpeg', 'image/png', 'image/gif', 'application/pdf']
+
+    has_attached_file :credit_bureaus_importer, styles: { medium: "300x300>", thumb: "100x100>" }
+    validates_attachment_content_type :credit_bureaus_importer, content_type: ['image/jpeg', 'image/png', 'image/gif', 'application/pdf']
+
+    has_attached_file :environment_social_audit, styles: { medium: "300x300>", thumb: "100x100>" }
+    validates_attachment_content_type :environment_social_audit, content_type: ['image/jpeg', 'image/png', 'image/gif', 'application/pdf']
+
+    has_attached_file :project_memorandum_exporter_business, styles: { medium: "300x300>", thumb: "100x100>" }
+    validates_attachment_content_type :project_memorandum_exporter_business, content_type: ['image/jpeg', 'image/png', 'image/gif', 'application/pdf']
+
+    has_attached_file :additional_document, styles: { medium: "300x300>", thumb: "100x100>" }
+    validates_attachment_content_type :additional_document, content_type: ['image/jpeg', 'image/png', 'image/gif', 'application/pdf']
     
 end
