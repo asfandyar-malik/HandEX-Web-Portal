4ff13b04913cca291d008c6317e220bfb635fd6e
Asfandyar Malik <asfandyarashraf@hotmail.com>
2019-05-31 13:40:34 +0200

    Add more fields

--- db/migrate/20190531113704_add_new_columns_to_table.rb
+++ db/migrate/20190531113704_add_new_columns_to_table.rb
@@ -3,5 +3,6 @@ class AddNewColumnsToTable < ActiveRecord::Migration[5.2]
     add_column :export_applications, :importer_tax_id, :string
     add_column :export_applications, :explain_sensitive_area_type, :string
     add_column :export_applications, :explain_special_structure, :string
+    add_column :export_applications, :exporter_date_founded, :string
   end
 end
