40368e8daedb82508164e7628c14e1c111534620
hamid4 <hamid@handex.co>
2019-06-04 22:07:10 +0200

    BACK-388
    Adding datapoint:
    is_fully_responsible_for_project
    db migration added.

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2019_06_04_200447) do
 
-ActiveRecord::Schema.define(version: 2019_06_04_191916) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -423,14 +423,6 @@ ActiveRecord::Schema.define(version: 2019_06_04_191916) do
     t.boolean "is_exporting_to_private_sector", default: false
     t.boolean "is_company_controlled_by_mother_company", default: false
     t.string "importer_mother_company_industry"
-    t.boolean "exporter_offers_servicing_for_goods", default: false
-    t.string "does_delivery_affect_sensitive_areas"
-    t.boolean "are_goods_overhauled", default: false
-    t.boolean "does_corruption_declaration_appy", default: false
-    t.string "importer_tax_id"
-    t.string "explain_sensitive_area_type"
-    t.string "explain_special_structure"
-    t.string "exporter_date_founded"
     t.string "agreed_payments_amount_payment_term_short_2"
     t.string "agreed_payments_output_payment_term_short_2"
     t.string "agreed_payments_time_payment_term_short_2"
@@ -460,6 +452,7 @@ ActiveRecord::Schema.define(version: 2019_06_04_191916) do
     t.string "explain_special_structure"
     t.string "exporter_date_founded"
     t.string "project_detailed_description"
+    t.string "is_fully_responsible_for_project"
     t.index ["user_id"], name: "index_export_applications_on_user_id"
   end
 
