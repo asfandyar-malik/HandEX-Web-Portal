70f674c1a296a2405727aa58feaa6952bdef333f
Asfandyar Malik <asfandyarashraf@hotmail.com>
2019-06-13 16:43:59 +0200

    Fixing a migration

--- db/migrate/20190613143738_fixing_column_names.rb
+++ db/migrate/20190613143738_fixing_column_names.rb
@@ -1,6 +1,6 @@
 class FixingColumnNames < ActiveRecord::Migration[5.2]
   def change
-    remove_column :multiple_deliveries, :payment_relevant_milestones
-    add_column :multiple_deliveries, :payment_relevant_milestone
+    remove_column :multiple_shipments, :payment_relevant_milestones
+    add_column :multiple_shipments, :payment_relevant_milestone
   end
 end
